bc34e48696b2543eef44d1b8834f1b7a33b0c7b8
Bert Belder <bertbelder@gmail.com>
2018-06-01 16:41:16 -0700

    build: make upload_website work again

--- tools/upload_website.js
+++ tools/upload_website.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const run = require("./run");
 const { execSync } = require("child_process");
-run.sh("./tools/website.js prod build");
+run.sh("./tools/build.js clean prod");
 // pip install awscli
 execSync(
   "aws s3 sync build/website/ s3://propelml.org " +
-    "--follow-symlinks --delete",
+    " --delete",
   { stdio: "inherit" }
 );
